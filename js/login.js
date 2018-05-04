348920f006a8c7bd5a55861f92ccf2a5f3fedfdf
Your Name <you@example.com>
2018-05-03 21:06:47 -0700

    adding a login page

---
+++ js/login.js
@@ -0,0 +1,38 @@
+// login java
+
+var counter = 0;
+
+function login() {
+  var username = document.getElementById('username');
+  var password = document.getElementById('password');
+  if (username.value == 'denise' && password.value == 'denise123') {
+    window.location.href = './login.html';
+    counter = 0;
+  } else {
+    counter++;
+    if (counter >= 5) {
+      window.location.href = './error.html';
+    } else {
+      console.log('counter.....', counter);
+      // perform error handling
+      var span = document.getElementById('error');
+      handleError(span, counter);
+    }
+  }
+}
+
+// set and display error 
+function handleError(span, counter) {
+  var errMsg = `Login error! Please try again! You have ${5 - counter} Tries left!`;
+  span.innerHTML = errMsg;
+  span.style = 'display:block;';
+  resetError(span);
+  return errMsg;
+}
+
+// reset error after 3 seconds
+function resetError(span) {
+  setTimeout(function () {
+    span.style = 'display:none';
+  }, 3000);
+}
